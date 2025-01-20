fec7b020b0dc6c1931885c8d561bf908c8defa78
Yazan Chama <ychama15@gmail.com>
2025-01-19 19:03:31 -0700

    Removing moved folders

--- src/app/appContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AppContext = React.createContext({
-  routes: [],
-  user: null,
-  setUser: () => {},
-});
-
-export default AppContext;
--- src/matx/components/MatxSidenav/MatxSidenavContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MatxSidenavContent = ({ children }) => (
-  <div className="matx-sidenav-content h-100">{children}</div>
-);
-
-export default MatxSidenavContent;
